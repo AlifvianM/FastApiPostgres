e4807c7725d3a41a43f4b32cf8be3c6bec0da0c9
Alifvian Marco <alifvianmarco@192.168.1.5>
2022-02-08 12:40:53 +0700

    first checkpoint

---
+++ app/main.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+from app.db import database, User, Item
+
+
+app = FastAPI(title="FastAPI, Docker, and POSTGRESQL")
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+@app.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+    # create a dummy entry
+    await User.objects.get_or_create(email="test@test.com")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
